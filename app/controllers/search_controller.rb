d9da28466e52673f4367715e00016de8f20350f7
Sejin Kim <froydroyce@gmail.com>
2019-08-26 09:42:30 -0600

    Create facade model and begin making API calls in postman

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    render locals: {
+      facade: StationSearch.new
+    }
   end
 end
---
+++ app/models/station_search.rb
@@ -0,0 +1,5 @@
+class StationSearch
+  def stations
+    get_json
+  end
+end
