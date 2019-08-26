383dc174c7eaed106ad89b66c1596e3f3b07ada6
Sejin Kim <froydroyce@gmail.com>
2019-08-26 09:30:47 -0600

    Create test and start writing wishful code on view

---
+++ spec/features/user_searches_for_locations_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'When I visit "/"' do
+    describe 'and I fill in the search form with 80203 and I click "Locate"' do
+      it "Then I should see a list of the 10 closest stations within 6 miles sorted by distance\n
+      And the stations should be limited to Electric and Propane\n
+      And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times" do
+        visit root_path
+
+        fill_in :q, with: 80203
+        click_button 'Locate'
+
+        expect(current_path).to eq(search_path)
+
+        expect(page).to have_css('.stations', count: 10)
+      end
+    end
+  end
+end
